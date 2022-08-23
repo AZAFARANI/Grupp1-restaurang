799064d9aee1f7282c6a03d0c7e4a3985c1a5508
ASleepingBazza <elias.e.fredriksson@gmail.com>
2022-08-23 09:42:55 +0200

    Created booking mongoose model

---
+++ backend/src/models/BookingModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const BookingSchema = new mongoose.Schema({
+    customerId: { type: mongoose.Types.ObjectId, required: true },
+    guestCount: { type: Number, required: true },
+    timestamp: { type: String, required: true },
+    allergies: { type: String, required: true },
+});
+
+const BookingModel = mongoose.model("bookings", BookingSchema);
+module.exports = BookingModel;
