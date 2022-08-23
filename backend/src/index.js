c83417630be0606727449624e23173ed3eb0fc5b
ASleepingBazza <elias.e.fredriksson@gmail.com>
2022-08-23 09:54:38 +0200

    Added bookings router

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,11 +7,20 @@ const app = express();
 
 app.use(express.json());
 
+// -------------------------------------------------------
+// ### ROUTERS ###
+const BookingsRouter = require("./routers/bookings");
+app.use("/bookings", BookingsRouter);
+// -------------------------------------------------------
+
+// -------------------------------------------------------
+// ### HOME ###
 app.get("/", (req, res) => {
     res.send({
         msg: "Hello World!",
     });
 });
+// -------------------------------------------------------
 
 app.listen(8000, () => {
     console.log("http://localhost:8000/");
