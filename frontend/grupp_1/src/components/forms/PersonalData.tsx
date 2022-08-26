8de964ace80097a8aae4463c5146675c7765820d
Filipengbergs <filip792@hotmail.com>
2022-08-26 13:56:27 +0200

    Moved personaldata form to its own component.

---
+++ frontend/grupp_1/src/components/forms/PersonalData.tsx
@@ -0,0 +1,110 @@
+import "../../scss/Booking.scss";
+import { Button } from "../Styled/CustomButton";
+import { StyledForm } from "../Styled/CustomForm";
+import { CustomSpan } from "../Styled/CustomSpan";
+import { CustomSpanDiv } from "../Styled/GlassmorphDiv";
+import { InputText } from "../Styled/StyledInputTypeText";
+
+export const PersonalData = () => {
+  return (
+    <>
+      <StyledForm>
+        <CustomSpanDiv
+          applyToNthChild="odd"
+          applyToNthChildTable="even"
+          flexDirectionTablet="column"
+          flexDirectionDesktop="row"
+          widthDesktop="80%"
+        >
+          <CustomSpanDiv flexDirectionTablet="column">
+            <CustomSpan
+              fontSize="24px"
+              fontSizeTablet="22px"
+              fontSizeDesktop="20px"
+            >
+              Din epost
+            </CustomSpan>
+            <InputText width="90%" height="50px" corner="10px"></InputText>
+          </CustomSpanDiv>
+          <CustomSpanDiv flexDirectionTablet="column" marginBottom="35px">
+            <CustomSpan
+              fontSize="24px"
+              fontSizeTablet="22px"
+              fontSizeDesktop="20px"
+            >
+              Mobilnummer
+            </CustomSpan>
+            <InputText width="90%" height="50px" corner="10px"></InputText>
+          </CustomSpanDiv>
+        </CustomSpanDiv>
+        <CustomSpanDiv
+          applyToNthChild="odd"
+          applyToNthChildTable="even"
+          flexDirectionDesktop="row"
+          widthDesktop="80%"
+          marginBottom="35px"
+        >
+          <CustomSpanDiv flexDirectionTablet="column" widthTablet="300px">
+            <CustomSpan
+              fontSize="24px"
+              fontSizeTablet="22px"
+              fontSizeDesktop="20px"
+            >
+              Förnamn
+            </CustomSpan>
+            <InputText
+              width="90%"
+              height="50px"
+              corner="10px"
+              widthTablet="95%"
+            ></InputText>
+          </CustomSpanDiv>
+          <CustomSpanDiv flexDirectionTablet="column" widthTablet="300px">
+            <CustomSpan
+              fontSize="24px"
+              fontSizeTablet="22px"
+              fontSizeDesktop="20px"
+            >
+              Efternamn
+            </CustomSpan>
+            <InputText
+              width="90%"
+              height="50px"
+              corner="10px"
+              widthTablet="95%"
+            ></InputText>
+          </CustomSpanDiv>
+        </CustomSpanDiv>
+        <CustomSpanDiv
+          applyToNthChild="even"
+          flexDirectionTablet="column"
+          widthDesktop="80%"
+        >
+          <CustomSpan
+            fontSize="24px"
+            fontSizeTablet="22px"
+            fontSizeDesktop="20px"
+          >
+            Har ni några allergier?
+          </CustomSpan>
+          <InputText
+            width="90%"
+            height="200px"
+            corner="10px"
+            widthDesktop="95%"
+          ></InputText>
+          <Button padding="20px 70px" background="#A3A380">
+            <CustomSpan
+              color="white"
+              fontSize="40px"
+              fontSizeTablet="30px"
+              fontSizeDesktop="20px"
+            >
+              Nästa
+            </CustomSpan>
+          </Button>
+        </CustomSpanDiv>
+      </StyledForm>
+    </>
+  );
+};
--- frontend/grupp_1/src/components/pages/Booking.tsx
+++ frontend/grupp_1/src/components/pages/Booking.tsx
@@ -1,16 +1,13 @@
 import "../../scss/Booking.scss";
-import { Button } from "../Styled/CustomButton";
-import { StyledForm } from "../Styled/CustomForm";
+import { PersonalData } from "../forms/PersonalData";
 import { StyledImage } from "../Styled/CustomImage";
 import { CustomSpan } from "../Styled/CustomSpan";
 import {
   ContainerDiv,
-  CustomSpanDiv,
   HeaderDiv,
   HeroDiv,
   SeperatorLine,
 } from "../Styled/GlassmorphDiv";
-import { InputText } from "../Styled/StyledInputTypeText";
 
 export const Booking = () => {
   return (
@@ -56,103 +53,7 @@ export const Booking = () => {
           <SeperatorLine></SeperatorLine>
         </HeaderDiv>
         <ContainerDiv flexDirection="column" widthDesktop="70%">
-          <StyledForm>
-            <CustomSpanDiv
-              applyToNthChild="odd"
-              applyToNthChildTable="even"
-              flexDirectionTablet="column"
-              flexDirectionDesktop="row"
-              widthDesktop="80%"
-            >
-              <CustomSpanDiv flexDirectionTablet="column">
-                <CustomSpan
-                  fontSize="24px"
-                  fontSizeTablet="22px"
-                  fontSizeDesktop="20px"
-                >
-                  Din epost
-                </CustomSpan>
-                <InputText width="90%" height="50px" corner="10px"></InputText>
-              </CustomSpanDiv>
-              <CustomSpanDiv flexDirectionTablet="column" marginBottom="35px">
-                <CustomSpan
-                  fontSize="24px"
-                  fontSizeTablet="22px"
-                  fontSizeDesktop="20px"
-                >
-                  Mobilnummer
-                </CustomSpan>
-                <InputText width="90%" height="50px" corner="10px"></InputText>
-              </CustomSpanDiv>
-            </CustomSpanDiv>
-            <CustomSpanDiv
-              applyToNthChild="odd"
-              applyToNthChildTable="even"
-              flexDirectionDesktop="row"
-              widthDesktop="80%"
-              marginBottom="35px"
-            >
-              <CustomSpanDiv flexDirectionTablet="column" widthTablet="300px">
-                <CustomSpan
-                  fontSize="24px"
-                  fontSizeTablet="22px"
-                  fontSizeDesktop="20px"
-                >
-                  Förnamn
-                </CustomSpan>
-                <InputText
-                  width="90%"
-                  height="50px"
-                  corner="10px"
-                  widthTablet="95%"
-                ></InputText>
-              </CustomSpanDiv>
-              <CustomSpanDiv flexDirectionTablet="column" widthTablet="300px">
-                <CustomSpan
-                  fontSize="24px"
-                  fontSizeTablet="22px"
-                  fontSizeDesktop="20px"
-                >
-                  Efternamn
-                </CustomSpan>
-                <InputText
-                  width="90%"
-                  height="50px"
-                  corner="10px"
-                  widthTablet="95%"
-                ></InputText>
-              </CustomSpanDiv>
-            </CustomSpanDiv>
-            <CustomSpanDiv
-              applyToNthChild="even"
-              flexDirectionTablet="column"
-              widthDesktop="80%"
-            >
-              <CustomSpan
-                fontSize="24px"
-                fontSizeTablet="22px"
-                fontSizeDesktop="20px"
-              >
-                Har ni några allergier?
-              </CustomSpan>
-              <InputText
-                width="90%"
-                height="200px"
-                corner="10px"
-                widthDesktop="95%"
-              ></InputText>
-              <Button padding="20px 70px" background="#A3A380">
-                <CustomSpan
-                  color="white"
-                  fontSize="40px"
-                  fontSizeTablet="30px"
-                  fontSizeDesktop="20px"
-                >
-                  Nästa
-                </CustomSpan>
-              </Button>
-            </CustomSpanDiv>
-          </StyledForm>
+          <PersonalData></PersonalData>
         </ContainerDiv>
       </HeroDiv>
     </>
