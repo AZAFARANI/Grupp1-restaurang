c421201514e388723b8e4bd39e764d1996fbe22e
ASleepingBazza <elias.e.fredriksson@gmail.com>
2022-08-29 10:24:18 +0200

    Added login route, added flex grow for main

--- frontend/grupp_1/src/components/Layout.tsx
+++ frontend/grupp_1/src/components/Layout.tsx
@@ -11,154 +11,161 @@ import { Link } from "react-router-dom";
 import { CustomDiv, CustomDivFooter } from "./Styled/CustomDiv";
 
 export const Layout = () => {
-  const [isClicked, setIsClicked] = useState<boolean>(false);
+    const [isClicked, setIsClicked] = useState<boolean>(false);
 
-  return (
-    <>
-      <MenuIcon
-        className={`${isClicked ? "hidden" : "visible"} open-menu`}
-        onClick={() => {
-          setIsClicked(!isClicked);
-        }}
-        icon={faBars}
-      />
-
-      <div className="nav-wrapper">
-        <div className={`${isClicked ? "slide-in" : "slide-out"} menu`}>
-          <div className="heading">
-            <CustomSpan
-              id="menuHeading"
-              fontSize="30pt"
-              color="white"
-              fontType="Montez"
-            >
-              Meny
-            </CustomSpan>
-
+    return (
+        <>
             <MenuIcon
-              icon={faXmark}
-              onClick={() => {
-                setIsClicked(false);
-              }}
+                className={`${isClicked ? "hidden" : "visible"} open-menu`}
+                onClick={() => {
+                    setIsClicked(!isClicked);
+                }}
+                icon={faBars}
             />
-          </div>
 
-          <div className="menuDiv">
-            <ul>
-              <Link
-                onClick={() => {
-                  setIsClicked(false);
-                }}
-                to={"/"}
-              >
-                <li>
-                  <CustomSpan color="white" fontSize="30px">
-                    Startsida
-                  </CustomSpan>
-                </li>
-              </Link>
-              <li>
-                <Link
-                  onClick={() => {
-                    setIsClicked(false);
-                  }}
-                  to={"/booking"}
+            <div className="nav-wrapper">
+                <div className={`${isClicked ? "slide-in" : "slide-out"} menu`}>
+                    <div className="heading">
+                        <CustomSpan
+                            id="menuHeading"
+                            fontSize="30pt"
+                            color="white"
+                            fontType="Montez"
+                        >
+                            Meny
+                        </CustomSpan>
+
+                        <MenuIcon
+                            icon={faXmark}
+                            onClick={() => {
+                                setIsClicked(false);
+                            }}
+                        />
+                    </div>
+
+                    <div className="menuDiv">
+                        <ul>
+                            <Link
+                                onClick={() => {
+                                    setIsClicked(false);
+                                }}
+                                to={"/"}
+                            >
+                                <li>
+                                    <CustomSpan color="white" fontSize="30px">
+                                        Startsida
+                                    </CustomSpan>
+                                </li>
+                            </Link>
+                            <li>
+                                <Link
+                                    onClick={() => {
+                                        setIsClicked(false);
+                                    }}
+                                    to={"/booking"}
+                                >
+                                    <CustomSpan color="white" fontSize="30px">
+                                        Boka bord
+                                    </CustomSpan>
+                                </Link>
+                            </li>
+
+                            <Link
+                                onClick={() => {
+                                    setIsClicked(false);
+                                }}
+                                to={"/contact"}
+                            >
+                                <li>
+                                    <CustomSpan color="white" fontSize="30px">
+                                        Kontaktsida
+                                    </CustomSpan>
+                                </li>
+                            </Link>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <main>
+                <Outlet />
+            </main>
+            <CustomDivFooter
+                className="contactContainer"
+                gap="0"
+                justifyContent="unset"
+                height="50vh"
+                backgroundImage="2"
+                backgroundColor="black"
+                padding="0px 0px"
+            >
+                <CustomDivFooter
+                    className="contactFirstSection"
+                    flexDirection="column"
+                    justifyContent="unset"
+                    height="40vh"
+                    backgroundImage="2"
+                    gap="70px"
+                    alignItems="center"
                 >
-                  <CustomSpan color="white" fontSize="30px">
-                    Boka bord
-                  </CustomSpan>
-                </Link>
-              </li>
+                    <div className="footerKontakt">
+                        <Link to={"/"}>
+                            <div className="contactItem notVisible contactIcon">
+                                <img
+                                    className="contactIcon2"
+                                    src="/svg/Vector-2.svg"
+                                ></img>
+                                <CustomSpan color="white" fontSize="1.3rem">
+                                    Hem
+                                </CustomSpan>
+                            </div>
+                        </Link>
 
-              <Link
-                onClick={() => {
-                  setIsClicked(false);
-                }}
-                to={"/contact"}
-              >
-                <li>
-                  <CustomSpan color="white" fontSize="30px">
-                    Kontaktsida
-                  </CustomSpan>
-                </li>
-              </Link>
-            </ul>
-          </div>
-        </div>
-      </div>
+                        <Link to={"/contact"}>
+                            <div className="contactItem">
+                                <img
+                                    className="contactIcon"
+                                    src="/svg/fluent_contact-card-48-regular.svg"
+                                ></img>
+                                <CustomSpan color="white" fontSize="1.3rem">
+                                    Kontakt
+                                </CustomSpan>
+                            </div>
+                        </Link>
 
-      <Outlet />
+                        <Link to={"/booking"}>
+                            <div className="contactItem notVisible">
+                                <img src="/svg/Vector.svg"></img>
+                                <CustomSpan color="white" fontSize="1.3rem">
+                                    Boka bord
+                                </CustomSpan>
+                            </div>
+                        </Link>
+                    </div>
 
-      <CustomDivFooter
-        className="contactContainer"
-        gap="0"
-        justifyContent="unset"
-        height="50vh"
-        backgroundImage="2"
-        backgroundColor="black"
-        padding="0px 0px"
-      >
-        <CustomDivFooter
-          className="contactFirstSection"
-          flexDirection="column"
-          justifyContent="unset"
-          height="40vh"
-          backgroundImage="2"
-          gap="70px"
-          alignItems="center"
-        >
-          <div className="footerKontakt">
-            <Link to={"/"}>
-              <div className="contactItem notVisible contactIcon">
-                <img className="contactIcon2" src="/svg/Vector-2.svg"></img>
-                <CustomSpan color="white" fontSize="1.3rem">
-                  Hem
-                </CustomSpan>
-              </div>
-            </Link>
-
-            <Link to={"/contact"}>
-              <div className="contactItem">
-                <img
-                  className="contactIcon"
-                  src="/svg/fluent_contact-card-48-regular.svg"
-                ></img>
-                <CustomSpan color="white" fontSize="1.3rem">
-                  Kontakt
-                </CustomSpan>
-              </div>
-            </Link>
-
-            <Link to={"/booking"}>
-              <div className="contactItem notVisible">
-                <img src="/svg/Vector.svg"></img>
-                <CustomSpan color="white" fontSize="1.3rem">
-                  Boka bord
-                </CustomSpan>
-              </div>
-            </Link>
-          </div>
-
-          <img
-            className="arrowUp"
-            src="/svg/bi_arrow-down-circle-fill.svg"
-          ></img>
-        </CustomDivFooter>
-        <CustomDivFooter
-          className="contactContainer"
-          flexDirection="row"
-          justifyContent="center"
-          backgroundColor="#cfc99bb8"
-          height="10vh"
-          backgroundImage="2"
-          alignItems="center"
-        >
-          <div>
-            <img src="/svg/ph_copyright-light.svg"></img>
-            <img className="contactIcon" src="/svg/Tramonto.svg"></img>
-          </div>
-        </CustomDivFooter>
-      </CustomDivFooter>
-    </>
-  );
+                    <img
+                        className="arrowUp"
+                        src="/svg/bi_arrow-down-circle-fill.svg"
+                    ></img>
+                </CustomDivFooter>
+                <CustomDivFooter
+                    className="contactContainer"
+                    flexDirection="row"
+                    justifyContent="center"
+                    backgroundColor="#cfc99bb8"
+                    height="10vh"
+                    backgroundImage="2"
+                    alignItems="center"
+                >
+                    <div>
+                        <img src="/svg/ph_copyright-light.svg"></img>
+                        <img
+                            className="contactIcon"
+                            src="/svg/Tramonto.svg"
+                        ></img>
+                    </div>
+                </CustomDivFooter>
+            </CustomDivFooter>
+        </>
+    );
 };
---
+++ frontend/grupp_1/src/components/pages/Login.tsx
@@ -0,0 +1,6 @@
+import { CustomDiv } from "../Styled/CustomDiv";
+import { HeaderDiv } from "../Styled/GlassmorphDiv";
+
+export default function Login() {
+    return <CustomDiv height="100%"></CustomDiv>;
+}
